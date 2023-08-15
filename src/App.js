9e9d899da0f9d0d5d39d4d4eced68dc6f6ec5de8
frankothe196 <kipkalisbartoo@gmail.com>
2023-08-15 14:05:43 +0300

    GLTF loading and controls complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+// Import glb/gltf loader
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useLoader } from '@react-three/fiber'
+
+import GLB_Model from './blender_model_export.glb'
 
 function App() {
 
   const ref = useRef()
-      
-  // Init renderer
-  const renderer = new THREE.WebGLRenderer({antialias:true})
+  
+  // Init renderer 
+  const renderer = new THREE.WebGLRenderer({alpha:true,antialias:true})
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.shadowMap.enabled = true
 
   // Init Scene
   const scene = new THREE.Scene()
@@ -20,19 +26,52 @@ function App() {
   scene.add(gridHelper)
 
   // Add a square
-  const geometry = new THREE.BoxGeometry(1,1,1)
-  const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-  const cube = new THREE.Mesh(geometry,material)
-  
-  scene.add(cube)
+  const Model_3D = useLoader(GLTFLoader, GLB_Model)
+
+  // const geometry = new THREE.BoxGeometry(1,1,1)
+  console.log(Model_3D) // This will help you understand how the 3D model was structured and how to work with it
+  const geometry = Model_3D.nodes.building.geometry
+  const material = new THREE.MeshPhongMaterial( { color: 0x909090, dithering: true } );
+  const model_mesh = new THREE.Mesh(geometry,material)
+  model_mesh.castShadow = true; //default is false
+  model_mesh.receiveShadow = true; //default
+  model_mesh.scale.set(0.1,0.1,0.1) // its alittle too large lets scale it down
+  model_mesh.rotation.y=0.9
+  model_mesh.position.set(0,0,-5)
+  // Useful code, note needed anymore but ill leave it here for future
+
+  // Model_3D.nodes.building.traverse(function(node) {
+  //   if(node.isMesh) {
+  //     // console.log(node)
+	// 		node.castShadow = node.receiveShadow = true;
+	// 	}
+	// });
+
+  scene.add(model_mesh)
+
 
   // Add lights 
-  const AmbientLight = new THREE.AmbientLight(0x404040)
+  const AmbientLight = new THREE.AmbientLight(0x404040,1)
   scene.add(AmbientLight)
+
+  // Add point light
+  const light = new THREE.PointLight( 0xffffff, 150, 100 );
+  light.position.set( 15, 15, 15 );
+  light.castShadow = true; // default false
+
+	light.shadow.mapSize.width = 1024;
+	light.shadow.mapSize.height = 1024;
+	light.shadow.camera.near = 1;
+	light.shadow.camera.far = 500;
+
+  const pointLightHelper = new THREE.PointLightHelper( light, 1 );
+
+  scene.add( light );
+  scene.add( pointLightHelper );  
 
   // Init Camera
   const camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,1,1000)
-  camera.position.set(5,3,5)
+  camera.position.set(8,5,8)
 
   // Init Controls
   const controls = new OrbitControls(camera, renderer.domElement)
